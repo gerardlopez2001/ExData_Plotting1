6652685afbf7b1036cf6a2b4d66a42b98cbf3457
Gerardlopez2001 <gerardlopez2001@gmail.com>
2014-10-11 15:11:49 +0700

    final draft

--- getPowerData.R
+++ getPowerData.R
@@ -1,5 +1,26 @@
 # setwd("~/School/JH/04 Exploratory Data PowerData/Projects/ExData_Plotting1/ExData_Plotting1")
 
+
+if (!file.exists("household_power_consumption.txt")){
+        
+
+fileUrl<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+# create a temporary directory
+td = tempdir()
+# create the placeholder file
+tf = tempfile(tmpdir=td, fileext=".zip")
+# download into the placeholder file
+download.file(fileUrl, tf)
+# get the name of the first file in the zip archive
+fname = unzip(tf, list=TRUE)$Name[1]
+# unzip the file to the temporary directory
+unzip(tf, files=fname, exdir=td, overwrite=TRUE)
+# fpath is the full path to the extracted file
+fpath = file.path(td, fname)
+#copy to local
+file.copy(fpath, "household_power_consumption.txt")
+}
+
 #get data
 data <- read.table("household_power_consumption.txt", sep=";", header = TRUE)
 PowerData <- data[(data$Date == "2/2/2007" | data$Date == "1/2/2007"),]
